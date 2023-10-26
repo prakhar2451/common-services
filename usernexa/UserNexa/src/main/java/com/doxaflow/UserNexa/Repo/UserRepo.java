e32477394460296ad1c25955eba881c1f9221ef1
prakhar2451 <38582421+prakhar2451@users.noreply.github.com>
2023-10-26 19:06:34 +0530

    Revert "UserNexa project setup done and created Entity,Repo, Service clas"
    This reverts commit db1b8ed24387f9015fba390e811bd09dd7ce92e6.

--- usernexa/UserNexa/src/main/java/com/doxaflow/UserNexa/Repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.doxaflow.UserNexa.Repo;
-
-import com.doxaflow.UserNexa.Entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User,Long> {
-}
